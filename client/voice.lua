fc864f49f0834e73df5239502d09fa20f7ef01d3
eladgith <eladgreen125@gmail.com>
2026-07-07 22:31:09 +0300

    fix: oopsie, forgot to include important voice events

--- client/voice.lua
+++ client/voice.lua
@@ -31,5 +31,40 @@ function IsUsingRadio()
     return false
 end
 
+local lastRadioState = false
+
+local function SetRadioState(isUsingRadio)
+    isUsingRadio = isUsingRadio == true
+
+    if lastRadioState == isUsingRadio then
+        return
+    end
+
+    lastRadioState = isUsingRadio
+    TriggerEvent('sd_lib:radioStateChanged', isUsingRadio)
+end
+
+RegisterNetEvent('pma-voice:radioActive', function(isUsingRadio)
+    if Config.VoiceSystem == 'pma-voice' then
+        SetRadioState(isUsingRadio)
+    end
+end)
+
+RegisterNetEvent('SaltyChat_RadioTrafficStateChanged', function(_primaryReceive, primaryTransmit, _secondaryReceive, secondaryTransmit)
+    if Config.VoiceSystem == 'saltychat' then
+        SetRadioState(primaryTransmit == true or secondaryTransmit == true)
+    end
+end)
+
+CreateThread(function()
+    if Config.VoiceSystem == 'pma-voice' and GetResourceState('pma-voice') ~= 'missing' then
+        lastRadioState = LocalPlayer.state.radioTalking == true
+
+        AddStateBagChangeHandler('radioTalking', ('player:%s'):format(GetPlayerServerId(PlayerId())), function(_, _, value)
+            SetRadioState(value)
+        end)
+    end
+end)
+
 exports('GetRadioChannel', GetRadioChannel)
 exports('IsUsingRadio', IsUsingRadio)
